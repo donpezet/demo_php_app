5e995f881e00c3fb1fd5648174ae71ee457cb13f
Don Pezet <don@itpro.tv>
2018-11-28 22:49:48 -0500

    Install Apache/PHP

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,15 @@
+package 'httpd' do
+  action :install
+end
+
+package 'php' do
+  action :install
+end
+
+service 'httpd' do
+  action :start
+end
+
 template '/var/www/html/index.php' do
   source 'index.php.erb'
 end
